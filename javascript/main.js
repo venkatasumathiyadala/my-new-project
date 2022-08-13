7db75ee2a6a4c2765c4f08340e037fe9c2dbefe0
venkatasumathiyadala <21bq5a0527@vvit.net>
2022-08-13 16:26:56 +0530

    lab

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+Main.js
+function drag(e){
+ e.dataTransfer.setData('image',e.target.id);
+}
+function allowDrop(e){
+ e.preventDefault();
+}
+function drop(e){
+ e.preventDefault();
+ var data = e.dataTransfer.getData('image');
+ e.target.appendChild(document.getElementById(data));
+}
